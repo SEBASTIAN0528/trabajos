8360b256592a05d01a3d7f39393f4ff9dd664517
sebastian0528 <sebastian.ibague@globant.com>
2018-08-31 09:11:52 -0500

    visual basic

---
+++ MYCricketers/MYCricketers/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MYCricketers.MvcApplication" Language="C#" %>
---
+++ arraylist/arraylist/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList nombre = new ArrayList();
+
+            nombre.Add("sebastian");
+            nombre.Add("ibague");
+            nombre.Add(17);
+            nombre.Add(false);
+
+            Console.WriteLine(nombre[0]);
+            Console.WriteLine(nombre[1]);
+            Console.WriteLine(nombre[2]);
+            Console.WriteLine(nombre[3]);
+
+            Console.ReadKey();
+
+        }
+    }
+}
